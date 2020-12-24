5436213da8b175f70d3d80066965f18ba278d5f5
GT Fari <guideg6@gmail.com>
2020-12-24 11:39:11 +0200

    add: Http module fetching from jsonplacehldr

--- src/app/services/todo.service.ts
+++ src/app/services/todo.service.ts
@@ -1,29 +1,18 @@
 import { Injectable } from '@angular/core';
+import { HttpClient, HttpHeaders} from '@angular/common/http'
+import {Todo} from '../models/Todo'
+import { Observable } from 'rxjs';
 
 @Injectable({
   providedIn: 'root'
 })
 export class TodoService {
+  todosUrl:string = 'https://jsonplaceholder.typicode.com/todos'
+  todosLimit = '?_limit=5'
 
-  constructor() { }
+  constructor(private http:HttpClient) { }
 
-  getTodos () {
-    return [
-      {
-        id: 1,
-        title: 'todo 1',
-        completed: false
-      },
-      {
-        id: 2,
-        title: 'todo two',
-        completed: true
-      },
-      {
-        id: 3,
-        title: 'todo three',
-        completed: false
-      },
-    ]
+  getTodos():Observable<Todo[]> {
+    return this.http.get<Todo[]>(`${this.todosUrl}${this.todosLimit}`);
   }
 }
